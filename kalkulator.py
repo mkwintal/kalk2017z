997f4b47a1c6890585530c3b10d65bcb8a69c917
Michal <m.kwintal91@gmail.com>
2017-06-03 17:53:20 +0200

    rozwiazalem konflikt

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,10 @@
 def dodawanie (a, b):
 	wynik = a + b 
 	return wynik
+def get_info() :
+		print('to jest prosty kalkulator')
+
+get_info()
 print ('Wprowadz pierwsza liczbe')
 z1 = int(input())
 print ('Wprowadz druga liczbe')
