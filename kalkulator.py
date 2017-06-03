a2aa9d32b203bb3729612692227829aefdae2999
Michal <m.kwintal91@gmail.com>
2017-06-03 17:40:51 +0200

    nowa funkcja dodawania gotowa do review

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,8 @@
 def dodawanie (a, b):
 	wynik = a + b 
 	return wynik
-
-dodawanie()
+print ('Wprowadz pierwsza liczbe')
+z1 = int(input())
+print ('Wprowadz druga liczbe')
+z2 = int(input())
+print(dodawanie(z1, z2))
